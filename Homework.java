2a73bfa2bac482499d60c016aa93f4ea7920f616
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2022-10-10 18:47:54 +0300

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Homework {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("1). ");
+
+        String big = scanner.nextLine();
+
+
+        int num = big.length();
+        System.out.println(num);
+
+        if (num >= 10){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+        System.out.print("2). ");
+
+        String startEnd = scanner.nextLine();
+
+
+
+        int a = startEnd.charAt(0);
+        int b = startEnd.charAt(startEnd.length() - 1);
+
+        if (a == b){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+
+    }
+}
