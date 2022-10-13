1daba42b77ccc2740a551e26e7c0d9218eee9b2f
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2022-10-13 18:57:14 +0300

    Add files via upload

---
+++ Dz.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Dz {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner. nextInt();
+
+        if (number == 0) {
+            System.out.println("ошибок не обнаружено");
+        }
+        else {
+            System.out.println("ERROR");
+            System.out.println("ERROR");
+            System.out.println("ERROR");
+        }
+
+
+
+
+
+
+    }
+}
