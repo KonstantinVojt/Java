ec331b6428975bfdf90410260198ae126886e17f
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2022-10-22 08:44:36 +0300

    Add files via upload

---
+++ Ternanry.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Ternanry {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        String l = "Я начинаю разбираться с циклами";
+
+        while (number >= 1) {
+            number--;
+            System.out.println(l);
+        }
+
+
+
+
+
+
+
+
+
+        //int number = scanner.nextInt();
+
+        //for (int i = 0; i < number; i += 2) {
+            //if (i % 10 == 2 || i % 10 == 6) {
+
+                //System.out.println(i);
+
+
+            }
+
+        }
+
+
+
+
