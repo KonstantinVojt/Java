74edbc4937c29d533a0ae87c5bf0917e067701fb
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2023-01-08 17:06:08 +0300

    Add files via upload

---
+++ VariousTasks/AverageValue.java
@@ -0,0 +1,17 @@
+package VariousTasks;
+
+public class AverageValue {
+
+    public int fundStateTrue(int first, int second) {
+        int result = (first + second) / 2;
+        return result;
+    }
+
+    public int fundStateFalse(int first, int second, int third) {
+        int result = (first + second + third) / 3;
+        return result;
+    }
+}
+
+
+
