d347cc6c3f9a933627dbf6a42ab9a715ef58e70b
KonstantinVojt <114500793+KonstantinVojt@users.noreply.github.com>
2023-03-01 23:11:12 +0300

    Add files via upload

---
+++ NewTask/Main.java
@@ -0,0 +1,18 @@
+package NewTask;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сообщение: ");
+
+        String massage = scanner.nextLine();
+
+        Encryption.massage(massage);
+    }
+    // евирП,т имр
+    // ямерВ дебоьта
+}
